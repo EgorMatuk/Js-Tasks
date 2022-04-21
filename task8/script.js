1ed825393a105bc3c0fd09a9d97d31aa50ba99d1
EgorMatuk <egorgorden@gmail.com>
2022-04-21 16:00:03 +0300

    newIn_8Task

--- task8/script.js
+++ task8/script.js
@@ -1,5 +1,8 @@
 function HashStorageFunc(){
-    let cars = {a:"Yeep", b:"sdsdsd"};
+    let cars = {
+        Маргарита:["Напиток БОМБА!","images/drinksImages/Margarita.jpg"], 
+        CocaCola:["Короче, делается из жука какого то...","images/drinksImages/CocaCola.jpg"], 
+    };
 
     this.addValue = function(key, value){
         cars[key] = value;
@@ -20,35 +23,84 @@ function HashStorageFunc(){
 
     //метод на вывод всего содержимого(в моем случае фотки машин)
     this.getAllValue = function(){
-        let valueArray = [];
-        for (const key in cars) {
-            valueArray.push(cars[key]);
-        }
-        return valueArray;
+        return cars;
     }
 }
 
 
 $(document).ready(function(){
-    let hash = new HashStorageFunc();
-    // hash.addValue("c","FEEEE");
-    // console.log(hash.getValue("c"));
-    // console.log(hash.getAllValue());
     
-    $('.addVal-click').click(function(){
-       
+    let drinkStorage  = new HashStorageFunc();
+    
+    
+    //Вывод информации обо всех объектах
+    $('.getAllValue-click').click(function(){
+        $('.drinks-container').empty();
+        let object = drinkStorage.getAllValue();
+        for (const key in object) {
+            $('.drinks-container').append(`
+                <div class = "drink-item">
+                    <h2>${key}</h2>
+                    <img src = "${object[key][1]}" alt = "НЕТ(">
+                    <p>${object[key][0]}</p>
+                </div>
+            `);
+        }
     });
 
-    $('.addVal-click').click(function(){
-       
+
+
+    //Удаление объекта
+    $('.deleteValue-click').click(function(){
+        $('.drinks-container').empty();
+        $('.drinks-container').append(`
+            <input class = "delete-input" type="text" placeholder="Название">
+            <button type="button" class="deleteItem">Удалить Напиток</button>
+        `);
+        $('.deleteItem').click(function(){
+            drinkStorage.deleteValue($('.delete-input').val());
+            $('.drinks-container').empty();
+        });
+        alert('Удаление прошло успешно)');
     });
 
-    $('.addVal-click').click(function(){
-       
+
+    //Вывод информации об объекте
+    $('.getInfo-click').click(function(){
+        $('.drinks-container').empty();
+        $('.drinks-container').append(`
+            <input class = "aboutItem-input" type="text" placeholder="Название">
+            <button type="button" class="aboutItem">Получить информацию</button>
+        `);
+            $('.aboutItem').click(function(){
+                let info = drinkStorage.getValue($('.aboutItem-input').val());
+                if(info){
+                    $('.drinks-container').empty();
+                    $('.drinks-container').append(`<p>${info[0]}</p>`);
+                }else{
+                    alert('Напиток не найден(');
+                }
+            });   
     });
 
+
+
+    //Добавление объекта
+    //на самом деле тут костыль(я еще не разобрался как добавить картинку через выбор файла), поэтому здесь надо вводить url картинки вручную, и тогда она добавится.
     $('.addVal-click').click(function(){
-       
+        $('.drinks-container').empty();
+        $('.drinks-container').append(`
+            <input class = "key-input" type="text" placeholder="Название">
+            <input class = "about-input" type="text" placeholder="Описание">
+            <input class = "image-input"  type="text" placeholder="Ссылка на фотографию(url)">
+            <button type="button" class="addItem">Добавить напиток</button>
+        `);
+        let paramArray = [];
+        $('.addItem').click(function(){
+            paramArray.push( $('.about-input').val());
+            paramArray.push( $('.image-input').val());
+            drinkStorage.addValue($('.key-input').val(), paramArray);
+        });
     });
 });
 
