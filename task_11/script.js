24258723c20c223e2b91d15242cb3abacc6fa9ee
EgorMatuk <egorgorden@gmail.com>
2022-05-03 23:17:53 +0300

    "===" change)

--- task_11/script.js
+++ task_11/script.js
@@ -3,6 +3,6 @@ function ChangeColor(Element) {
     var catName = document.querySelector('.card-image span');
     var aboutCat = document.querySelector('.card-content p');
     var changeImg = document.querySelector('.card-image img');
-	Element.innerHTML == 'cat2'? (changeImg.src ="./images/cat2.jpg", catName.innerHTML = "Cat2", aboutCat.innerHTML = "I am a beautiful cat №2") : onlyFirstImg;
-    Element.innerHTML == 'cat3'? (changeImg.src ="./images/cat3.jpg", catName.innerHTML = "Cat3", aboutCat.innerHTML = "I am a beautiful cat №3") : onlyFirstImg;
+	Element.innerHTML === 'cat2'? (changeImg.src ="./images/cat2.jpg", catName.innerHTML = "Cat2", aboutCat.innerHTML = "I am a beautiful cat №2") : onlyFirstImg;
+    Element.innerHTML === 'cat3'? (changeImg.src ="./images/cat3.jpg", catName.innerHTML = "Cat3", aboutCat.innerHTML = "I am a beautiful cat №3") : onlyFirstImg;
 }
