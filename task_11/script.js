f4044f2d22546d49fad07a554707696f75ddcd28
EgorMatuk <egorgorden@gmail.com>
2022-05-04 09:48:49 +0300

    task11End

--- task_11/script.js
+++ task_11/script.js
@@ -6,3 +6,36 @@ function ChangeColor(Element) {
 	Element.innerHTML === 'cat2'? (changeImg.src ="./images/cat2.jpg", catName.innerHTML = "Cat2", aboutCat.innerHTML = "I am a beautiful cat №2") : onlyFirstImg;
     Element.innerHTML === 'cat3'? (changeImg.src ="./images/cat3.jpg", catName.innerHTML = "Cat3", aboutCat.innerHTML = "I am a beautiful cat №3") : onlyFirstImg;
 }
+
+function addCat(){
+    var name = prompt("Введите имя котика:");
+    var country = prompt("Страна:");
+    var bthDate = prompt("Год рождения:");
+
+    document.querySelector('#cat-info').insertAdjacentHTML("beforeend",`
+        <tr>
+            <td>${name}</td>
+            <td>${country}</td>
+            <td>${bthDate}</td>
+        </tr>
+    `);
+}
+
+function darkTheme(){
+    var newImg = document.querySelector('#switcher img');
+
+    document.querySelector('body').style.backgroundColor != "black"? (
+        newImg.src = "./images/moon.svg",
+        document.querySelector('body').style.backgroundColor = "black",
+        document.querySelector('table').style.color = "white",
+        document.querySelector('#switcher').style.color = "white",
+        newImg.style.backgroundColor = "white"
+    ) : (
+        newImg.src = "./images/sun.svg",
+        document.querySelector('body').style.backgroundColor = "white",
+        document.querySelector('table').style.color = "black",
+        document.querySelector('#switcher').style.color = "black"
+    );
+
+    console.log(newImg.src);
+}
