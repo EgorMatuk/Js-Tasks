b6ad175f1da79a6868bcf95314d00b8fe7fe87ed
EgorMatuk <egorgorden@gmail.com>
2022-05-17 18:20:17 +0300

    task13new

---
+++ task_13/script.js
@@ -0,0 +1,12 @@
+// let form = document.getElementsByTagName('form');
+// form.onsubmit = function(){
+//     console.log('sdsdsd')
+// }
+
+// function formIsValid(){
+//     let inputs = document.querySelectorAll('input');
+//     inputs.forEach(function (input){
+//         !input.value? input.classList.add('error') : false; 
+//     });
+// }
+
