9c8372933e4980be66a2783d058bb9bae7709206
EgorMatuk <egorgorden@gmail.com>
2022-05-23 18:52:34 +0300

    ++

--- task_14/script.js
+++ task_14/script.js
@@ -1,84 +1,3 @@
-// var userForm = document.querySelector('#start-question');
-// var avatarContainer = document.querySelector('#start-question');
-// var userData = ['Joe', 'avatar'];
-// var questionBox = document.querySelector('#box-questions');
-// var questionForm = document.querySelector('.quiz-form');
-// var correctAnswers = ['B', 'D', 'B', 'A', 'C'];
-// var resultContainer = document.querySelector('.quiz__heading');
-
- 
-
-// avatarContainer.addEventListener('click', function(e){
-//     if(document.querySelectorAll('.show')){
-//         document.querySelectorAll('.show').forEach(function(item){
-//             item.classList.remove('show');
-//         })
-//     }
-//     //console.log(e.target);
-
-//     if(e.target.tagName === 'INPUT' && e.target.type === 'radio'){
-//         console.log(e.target.previousElementSibling);
-//         e.target.previousElementSibling.firstElementChild.classList.add('show');
-//     }
-// })
-
-// userForm.addEventListener('submit', function(e){
-//     var activeAvatar = document.querySelector('input[name="question2"]:checked');
-//     var activeName = userForm.elements.question1.value;
-//     var activeImg = activeAvatar.previousElementSibling.firstElementChild.src;
-//     userData = [activeName, activeImg];
-
-//     console.log(userData);
-
-//     userForm.classList.add('quiz');
-//     questionBox.classList.remove('quiz');
-
-//     scrollingUp();
-
-//     e.preventDefault();
-// })
-
-// questionForm.addEventListener('submit', function(e){
-//     var userInfo = document.querySelector('.name__user');
-//     var count = 0;
-//     var startResult = 0;
-
-//     var userAnswer = [
-//         questionForm.elements.q1,
-//         questionForm.elements.q2,
-//         questionForm.elements.q3,
-//         questionForm.elements.q4,
-//         questionForm.elements.q5
-//     ]
-
-//     userAnswer.forEach(function(item, index){
-//         if(item.value === correctAnswers[index]){
-//             count += 20;
-//             for(var i = 0; i < 4; i++){
-//                 var isChecked = item[i].checked;
-//                 if(isChecked){
-//                     item[i].parentElement.classList.add('correct')
-//                 }
-//             }
-//         }else{
-//             for(var i = 0; i < 4; i++){
-//                 var isChecked = item[i].checked;
-//                 if(isChecked){
-//                     item[i].parentElement.classList.add('wrong')
-//                 }
-//             }
-//         }
-//     })
-
-//     resultContainer.classList.add('show-quiz');
-
-//     userInfo.innerHTML = userData[0] + '<img src="'+ userData[1] +'" alt="user">' + count;
-
-//     scrollingUp();
-
-//     e.preventDefault();
-
-// })
 var user = {
     name:'User',
     avatar: document.querySelector('.show').src
@@ -88,6 +7,15 @@ var userForm = document.querySelector('#start-question');
 var mainWindow = document.querySelector('.heading');
 var questionWindow = document.querySelector('.quiz');
 var avatarContainer = document.querySelector('#start-question');
+
+var questBoxForm = document.querySelector('.quiz-form');
+var questionsContainers = document.querySelectorAll('.quiz-form__quiz');
+var answer = '';
+var userAnswers = [];
+var trueAnswers = ['B', 'D', 'B', 'A', 'C'];
+
+var resultBlock = document.querySelector('.quiz__heading');
+var userInfo = document.querySelector('.name__user');
 
 
 //Событие ВЫБОР АВАТАРКИ пользователя
@@ -115,20 +43,42 @@ userForm.addEventListener('submit', function(action){
     questionWindow.querySelector('.submit').classList.add('quiz');
 });
 
+document.getElementById('box-questions').addEventListener('click',function(event){
+    if(event.target.tagName === 'INPUT' && event.target.type === 'radio'){
+        answer = event.target.value;
+    }
+});
 
-var questionsContainers = document.querySelectorAll('.quiz-form__quiz');
 //Next button
 document.querySelector('.next').addEventListener('click', function(){
     var newQuestion;
     questionsContainers.forEach(function(question){
-        if(!question.classList.contains('quiz')){
+        if(!question.classList.contains('quiz') && answer !== ''){
             question.classList.add('answered');
             question.classList.add('quiz');
             newQuestion = question.nextElementSibling;
+            userAnswers.push(answer);
+            answer = '';
         }
     })
     newQuestion.classList.remove('quiz');
     islast(newQuestion);
+});
+
+//Вывод результата
+questBoxForm.addEventListener('submit',function(action){
+    userAnswers.push(answer);
+    var answersSet = new Set(userAnswers);
+    var result = trueAnswers.reduce((acc, v) => acc + answersSet.has(v), 0) / trueAnswers.length * 100 + '%';
+    questBoxForm.classList.add('quiz');
+    resultBlock.classList.remove('quiz__heading');
+    userInfo.innerHTML = `<button class="restart">Restart</button>` + user.name +' '+ result + ' right' + '</br>' + `<img src = "${user.avatar}" alt = "${user.name} photo"></img>`;
+    action.preventDefault();
+
+    //Рестарт страницы по клику
+    document.querySelector('.restart').addEventListener('click',function(){
+        window.location.reload();
+    });
 });
 
 //Проверка на последний вопрос
@@ -138,6 +88,3 @@ function islast(question){
         questionWindow.querySelector('.next').classList.add('quiz')
     ) : false;
 }
-
-
-
