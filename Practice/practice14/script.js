c03f7b14e9c1442f4ff6bd1f7d2f311fe10a9d5d
EgorMatuk <egorgorden@gmail.com>
2022-05-23 19:58:29 +0300

    +++

--- Practice/practice14/script.js
+++ Practice/practice14/script.js
@@ -1,46 +1,46 @@
-var ulList = document.querySelector('#todo-list');
+//task1
+document.getElementById('todo-list').addEventListener('click', function(event){
+    event.target.tagName === 'P'? event.target.classList.toggle('task-no-active') : false;
+});
 
-ulList.addEventListener('click', function(event){
-    //console.log(event.target.tagName);
-    if(event.target.tagName === 'P'){
-        event.target.classList.toggle('task-no-active');
-    }
-})
+//task2
+document.querySelector('.collection').addEventListener('click', function(event){
+    event.target.classList.contains('collection-item')? event.target.nextElementSibling.classList.toggle('answer-item-show') : false;
+});
 
-// Task 2
-
-//parent element   ul.collection
-
-//  (event.target.classList.contains('collection-item'))
-//  event.target.nextElementSibling
-
-
-// Task 3
-
-var btnOpen = document.querySelector('#open-modal');
-var modal = document.querySelector('#modal1');
-
-btnOpen.addEventListener('click', function(){
-    modal.classList.add('modal_open');
-})
-
+//task3
+document.querySelector('.row').addEventListener('click', function(event){
+    event.target.classList.contains('btn') ? document.getElementById('modal1').classList.toggle('modal_open') : false;
+});
 document.body.addEventListener('click', function(event){
-    console.log(event.target);
-    /*&& event.target !== modal*/
-    if(event.target !== btnOpen && !event.target.closest('#modal1')){
-        modal.classList.remove('modal_open');
-    }
-})
+    event.target.closest('#modal1') ? document.getElementById('modal1').classList.toggle('modal_open') : false;
+});
 
+//task4
+var mainPictue = document.querySelector('.photo img');
+var imagesContainer = document.querySelector('.container');
 
-// Task4
+imagesContainer.addEventListener('click', function(event){
+    event.target.parentElement.classList.contains('image') ? mainPictue.src = event.target.src : false;
+});
 
-var bigPicture = document.querySelector('.photo img');
-var boxImages = document.querySelector('.container');
-
-boxImages.addEventListener('click', function(event){
-    console.log(event.target);
-    if(event.target.parentElement.classList.contains('image')){
-        bigPicture.src = event.target.src;
-    }
-})
+//task5
+var list = document.querySelector('.second');
+var items = document.querySelectorAll('.second li');
+document.getElementById('sort').addEventListener('click', function(){
+    let sorted = [...items].sort(function(a, b) {
+		if(a.children[0].innerHTML > b.children[0].innerHTML){
+            return 1;
+        }
+        if(a.children[0].innerHTML < b.children[0].innerHTML){
+            return -1;
+        }
+        return 0;
+	});
+	
+	list.innerHTML = '';
+	
+	for (let li of sorted) {
+		list.appendChild(li);
+	}
+});
