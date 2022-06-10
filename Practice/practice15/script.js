48c1526fb2e9897e06ecb501d2600af26c54b802
EgorMatuk <egorgorden@gmail.com>
2022-06-10 18:30:08 +0300

    +++tasks

--- Practice/practice15/script.js
+++ Practice/practice15/script.js
@@ -1,67 +1,115 @@
- //task1
- var todoList = document.querySelector('#todo-list');
- todoList.addEventListener('click', function(event){
-    if(event.target.classList.contains('waves-effect')){
+//  task1
+
+var todoList = document.querySelector('#todo-list');
+
+todoList.addEventListener('click', function(event) {
+    if (event.target.classList.contains('waves-effect')) {
         var newTitle = prompt('Input new title!');
         event.target.previousElementSibling.innerHTML = newTitle;
     }
- });
+})
 
-//task2
- var area = document.querySelector('#numbers');
- area.addEventListener('click', function(event){
-    if(event.target.classList.contains('waves-light')){
-        var pow = document.querySelector('#numbers input');;
-        pow.value = Math.pow(event.target.innerHTML, 2);
+//task 3
+var ulList = document.querySelector('#menuList');
+var btnAdd = document.querySelector('#btnAdd');
+
+ulList.addEventListener('click', function(event) {
+    console.log(event.target);
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('task_is_done');
     }
- });
 
- //task3
- var menuList = document.getElementById('menuList');
- document.getElementById('btnAdd').addEventListener('click', function(){
-    var elements = menuList.getElementsByTagName('li');
-    menuList.insertAdjacentHTML('beforeend', `<li>task ${elements.length + 1}</li>`);
- });
+})
+var numberOfTask = 3;
+btnAdd.addEventListener('click', function(event) {
+        numberOfTask = ++numberOfTask
+        var newLi = document.createElement('li');
+        if (event.target === btnAdd) {
+            newLi.innerText = `task ${numberOfTask}`;
+            ulList.append(newLi);
+        }
+    })
+    //task 5
+var totalInput = document.getElementById('totalInput');
+var countSum = document.querySelector('#countSum');
+var currentTotal = document.getElementById('current-total');
+var price = document.querySelector('.price');
+var results = 0;
+document.addEventListener('click', function(event) {
 
- menuList.addEventListener('click', function(event){
-     event.target.tagName === 'LI' ? event.target.classList.toggle('line') : false;
- });
-
- //task5
- var eventArea = document.getElementById('current-cart');
- var totalInput = document.querySelector('.total');
- eventArea.addEventListener('click', function(event){
-    if(event.target.tagName === 'A' && event.target.innerHTML === '+1'){
-        ++totalInput.value;
-    }else if(event.target.tagName === 'A' && event.target.innerHTML === '-1' && totalInput.value != '0'){
-        --totalInput.value;
-    }else if(event.target.tagName === 'A' && event.target.innerHTML === 'Посчитать сумму'){
-        document.getElementById('current-total').innerHTML = totalInput.value * document.querySelector('.price').innerHTML;
+    if (event.target.dataset.counterIncr != undefined) {
+        totalInput.value.innerText = ++totalInput.value;
     }
- });
-
- //task6
- var itemsArea = document.getElementById('current-cart-items');
- itemsArea.addEventListener('click', function(event){
-    if(event.target.tagName === 'A' && event.target.innerHTML === '+1'){
-        ++event.target.previousElementSibling.value;
-    }else if(event.target.tagName === 'A' && event.target.innerHTML === '-1' && event.target.nextElementSibling.value != '0'){
-        --event.target.nextElementSibling.value;
-    }else if(event.target.tagName === 'A' && event.target.innerHTML === 'Посчитать сумму всех товаров'){
-        var items = itemsArea.querySelectorAll('.card-action');
-        var sum = 0;
-        items.forEach(element => {
-           sum += element.querySelector('.price').innerHTML * element.querySelector('.total').value;
-        });
-        itemsArea.querySelector('#current-total-items').innerHTML = sum;
+    if (event.target.dataset.counterDecr != undefined) {
+        if (totalInput.value === '0') {
+            return;
+        } else if (totalInput.value != '0')
+            totalInput.value.innerText = --totalInput.value;
     }
- });
- 
- //task7
- var inputWords = document.querySelector('input[name="input-info"]');
- inputWords.addEventListener('keyup', function(event){
-     if(event.code === 'Enter' && inputWords.value != ''){
-        inputWords.insertAdjacentHTML('afterend',`<p>${inputWords.value}</p>`);
-        inputWords.value = '';
-     }
- })
+    results = totalInput.value * price.innerHTML;
+})
+countSum.addEventListener('click', function() {
+    currentTotal.innerHTML = results;
+})
+
+// task6
+
+
+//task 6
+var inputTotal = document.querySelector('.total');
+
+var inputTotalEqualSum = document.querySelectorAll('.total');
+var card = document.querySelectorAll('.card');
+var countEqualSum = document.querySelector('#countEqualSum');
+var currentTotalItems = document.getElementById('current-total-items');
+// var price = document.querySelector('.price');
+
+
+
+
+document.addEventListener('click', function(event) {
+    var result = 0;
+    for (var i = 1; i < 5; i++) {
+
+        console.log(event.target);
+        if (card[i] == event.target.parentElement.parentElement) {
+            if (event.target.dataset.counterIncrease != undefined) {
+                inputTotalEqualSum[i].value.innerHTML = ++inputTotalEqualSum[i].value;
+            }
+            if (event.target.dataset.counterDecrease != undefined) {
+                if (inputTotalEqualSum[i].value === '0') {
+                    return;
+                } else if (inputTotalEqualSum[i].value != '0')
+                    inputTotalEqualSum[i].value.innerHTML = --inputTotalEqualSum[i].value;
+            }
+        }
+        result += inputTotalEqualSum[i].value * price.innerHTML;
+    }
+    countEqualSum.addEventListener('click', function() {
+        currentTotalItems.innerHTML = `${result}`;
+    })
+})
+
+// task7
+var inputEnter = document.getElementById('inputEnter');
+var enterField = document.querySelector('.enter_field');
+// inputEnter.addEventListener('keydown', function(event) {
+//     if (event.key === "Enter") {
+//         // Do Something, may be an 'Undo' operation
+//         // inputEnter.value 
+//         inputEnter.value = '';
+//     }
+// });
+
+inputEnter.addEventListener("keydown", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.code == 'Enter') {
+        console.log(event.key);
+        // Cancel the default action, if needed
+        enterField.classList.remove('enter_field');
+        enterField.innerHTML = inputEnter.value;
+        inputEnter.value = null;
+        // Trigger the button element with a click
+
+    }
+});
--- Practice/practice16/script.js
+++ Practice/practice16/script.js
@@ -1,19 +1,79 @@
+/**
+ Домашнее задание TASKS_16.
+ Вам необходимо из папки “homework16” реализовать задания №1,№2,№3, №4.
+ */
 
+task1();
+task2();
+task3();
+task4();
 
+/** TASK 1 */
+// Для всех input необходимо реализовать так,
+// чтобы они выводили свой value алертом при нажатии на любой из них,
+// но только по первому нажатию.
+// Повторное нажатие на input не должно вызывать реакции.
+function task1() {
+    document.getElementById('task-1-container').addEventListener('click', event => {
+        const target = event.target;
+        if (target.tagName === 'INPUT' && !target.dataset.checked) {
+            target.dataset.checked = 'true';
+            alert(target.value);
+        }
+    })
+}
 
- 
+/** TASK 2 */
+// Привяжите всем input следующее событие
+// - по потери фокуса каждый input выводит
+// свое value в абзац с текстом "Вы ввели (такой-то текст)...".
+function task2() {
+    document.querySelectorAll('#task-2-container input').forEach(input => {
+        input.addEventListener('blur', event => {
+            const pElement = document.createElement('p');
+            pElement.innerText = `Вы ввели "${input.value}"`;
+            input.parentNode.appendChild(pElement);
+        })
+    })
+}
 
-/**Task7 */
+/** TASK 3 */
+// Дана форма с id="sum-form".
+// В ней даны input, в них числа.
+// Дана кнопка. По нажатию на эту кнопку получите форму по ее id,
+// затем циклом переберите все input в ней и найдите сумму чисел из этих input.
+/**
+ * ПРИМЕЧАНИЕ: тк нет данных об способе выбора вывода данных выбран alert,
+ *      также принято решение остановить функциональность браузера по событию submit,
+ *      тк оно не требуется.
+ */
+function task3() {
+    document.getElementById('sum-form-btn')
+        .addEventListener("click", event => {
+                event.preventDefault();
+                // event.stopImmediatePropagation();
+                let sum = 0;
+                document
+                    .getElementById('sum-form')
+                    .querySelectorAll('input')
+                    .forEach(input => {
+                        sum += Number(input.value);
+                    });
+                alert(sum);
+            }
+        )
+}
 
-var searchList = [
-    'Мишка',
-    'Осьминожка',
-    'Акула',
-    'Авокадо',
-    'Моль Молли',
-    'Альпака',
-    'Пингвиненок',
-    'Моль',
-    'Пачакун',
-    'Ленивец'
-]
+/** TASK 4 */
+// Дан селект.
+// Дан input.
+// По изменению селекта выведите текст выбранного пункта в инпут.
+function task4() {
+    const container = document.getElementById('task-4-container');
+    const select = container.getElementsByTagName('select')[0];
+    const input = container.getElementsByTagName('input')[0];
+    select.addEventListener('change', event=>{
+        input.value = select.options[select.options.selectedIndex].innerText;
+    })
+}
+
