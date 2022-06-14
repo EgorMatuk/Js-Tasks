53b587fd2c48dd8dd9494d8c3c791f315b40d02a
Egor Matuk <esddss@gmail.com>
2022-06-14 21:28:08 +0300

    +++postGet

--- Practice/practice23/weather_app/script.js
+++ Practice/practice23/weather_app/script.js
@@ -1,5 +1,29 @@
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
- 
+const APP_ID = '6d89fc5c41786a00d4d546206d6b5d74';
+const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
+const getResurces = async(url) => {
+    try {
+        const res = await fetch(url);
+        return res.json();
+    } catch (error) {
+        throw new Error(`Error: ${error.status}`);
+    }
+}
 
-//https://api.openweathermap.org/data/2.5/forecast?q=Minsk&appid=4f82ea256c9b3cdbfa9b7d9d5ae06516
+const getWeatherInfo = async(place = 'Minsk') => {
+    const result = await getResurces(`${WEATHER_URL}?q=${place}&appid=${APP_ID}`);
+    return result;
+}
+
+const renderWeatherApp = async() => {
+    const currentPlace = document.getElementById('place').value.trim();
+    const data = await getWeatherInfo(currentPlace);
+    const currentTemp = document.querySelector('#current-temp');
+    const currentWind = document.querySelector('#current-wind');
+
+    currentTemp.innerHTML = Math.round(data.main.temp - 273);
+    currentWind.innerHTML = Math.ceil(data.wind.speed);
+}
+document.querySelector('#get-info').addEventListener('click', renderWeatherApp);
