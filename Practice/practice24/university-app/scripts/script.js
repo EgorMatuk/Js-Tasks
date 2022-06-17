abdab22a88637fb10a3bcaff54cb694217061686
EgorMatuk <egorgorden@gmail.com>
2022-06-17 21:56:32 +0300

    +++PreLoader

--- Practice/practice24/university-app/scripts/script.js
+++ Practice/practice24/university-app/scripts/script.js
@@ -1,5 +1,11 @@
+const togglePreloader = () => {
+    const preloader = document.querySelector('.wrap-preloader');
+    preloader.classList.toggle('preloader-show');
+}
+
 const renderUniverList = async() =>{
     const univerCurrent = document.getElementById('current-tour').value;
+    togglePreloader();
     const dataUniver = await getUniversList(univerCurrent);
     const uList = document.querySelector('#list-matches');
 
@@ -13,6 +19,7 @@ const renderUniverList = async() =>{
             </a>
         `
     });
+    togglePreloader();
 }
 
 document.getElementById('current-tour').addEventListener('change',renderUniverList);
